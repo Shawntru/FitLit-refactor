3766182041820d53d94ef67139b029272a1048bb
Shawn Truesdale <68252181+Shawntru@users.noreply.github.com>
2020-10-22 17:04:03 -0600

    Revert "Complete linting w/ air BnB linter"

--- src/User.js
+++ src/User.js
@@ -7,12 +7,11 @@ class User {
     this.strideLength = userDetails.strideLength;
     this.dailyStepGoal = userDetails.dailyStepGoal;
     this.friends = userDetails.friends;
-  }
 
+  }
   getFirstName() {
     return this.name.split(' ', 1).join();
   }
-
   getFriendsNames(userStorage) {
     return this.friends.map((friendId) => (userStorage.getDataFromID(friendId).name));
   }
