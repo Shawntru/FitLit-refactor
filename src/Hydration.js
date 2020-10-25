babea3c74589ebcac68329f1c5a6a10461b11606
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-10-24 20:22:05 -0600

    Fix calculate first week ounces and add helper function

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,12 +1,12 @@
 class Hydration {
   constructor(hydrationData, user) {
-    this.userID = user.id;
+    this.userId = user.id;
     this.hydrationData = this.getHydrationData(hydrationData);
   }
   
   getHydrationData(hydrationData) {
     let filteredData = hydrationData.filter(data => {
-      return this.userID === data.userID
+      return this.userId === data.userId
     })
       return filteredData.map( data => {
        return {date: data.date, numOunces: data.numOunces}
@@ -25,17 +25,32 @@ class Hydration {
      let daily = this.hydrationData.find(data => {
      return date === data.date
      })
-  
      return daily.numOunces
-  }
-  //complete refactoring following tests after refactoring userRepo
-  calculateFirstWeekOunces(userRepo, id) {
-    return userRepo.getFirstWeek(id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
-  }
-
-  calculateRandomWeekOunces(date, id, userRepo) {
-    return userRepo.getWeekFromDate(date, id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
-  }
+   }
+  
+  calculateFirstWeekOunces(date) {
+    let weekData = this.findGivenWeek(date);
+      let weekHydration = weekData.reduce((wholeWeek, day) => {
+        wholeWeek.push(day.numOunces)
+        return wholeWeek
+      }, [])
+      return weekHydration
+    }
+  
+    findGivenWeek(date) {
+      let sorted = this.hydrationData.sort((a, b) => {
+        return Date.parse(a.date) - Date.parse(b.date)
+      })
+        let endDate = sorted.findIndex(data =>{
+        return data.date === date;
+      })
+      let startDate = endDate;
+      if (startDate < 6){
+         startDate = 6; 
+       }
+      return sorted.slice((startDate - 6), (endDate + 1));
+    }
+  
 }
 
 export default Hydration;
