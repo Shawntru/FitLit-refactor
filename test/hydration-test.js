c3ad220f2273fe088e74981d5e502977c9ff85df
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-10-26 11:45:41 -0600

    Fix capitalization on user ID

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -21,27 +21,27 @@ describe('Hydration', () => {
     }
     user = new User(userData)
     hydrationData = [{
-      userId: 1,
+      userID: 1,
       date: '2019/06/15',
       numOunces: 37,
     },
     {
-      userId: 2,
+      userID: 2,
       date: '2019/06/15',
       numOunces: 38,
     },
     {
-      userId: 3,
+      userID: 3,
       date: '2019/05/09',
       numOunces: 1,
     },
     {
-      userId: 4,
+      userID: 4,
       date: '2019/04/15',
       numOunces: 36,
     },
     {
-      userId: 2,
+      userID: 2,
       date: '2018/10/23',
       numOunces: 34,
     },
@@ -51,12 +51,12 @@ describe('Hydration', () => {
       numOunces: 39,
     },
     {
-      userId: 3,
+      userID: 3,
       date: '2018/03/30',
       numOunces: 2,
     },
     {
-      userId: 4,
+      userID: 4,
       date: '2018/02/01',
       numOunces: 28,
     },
@@ -67,52 +67,52 @@ describe('Hydration', () => {
       numOunces: 30,
     },
     {
-      userId: 3,
+      userID: 3,
       date: '2016/05/14',
       numOunces: 3,
     },
     {
-      userId: 2,
+      userID: 2,
       date: '2016/04/27',
       numOunces: 40,
     },
     {
-      userId: 4,
+      userID: 4,
       date: '2019/03/15',
       numOunces: 35,
     },
     {
-      userId: 4,
+      userID: 4,
       date: '2019/09/21',
       numOunces: 61,
     },
     {
-      userId: 4,
+      userID: 4,
       date: '2019/09/20',
       numOunces: 40,
     },
     {
-      userId: 4,
+      userID: 4,
       date: '2019/09/19',
       numOunces: 30,
     },
     {
-      userId: 4,
+      userID: 4,
       date: '2019/09/18',
       numOunces: 40,
     },
     {
-      userId: 4,
+      userID: 4,
       date: '2019/09/17',
       numOunces: 40,
     },
     {
-      userId: 4,
+      userID: 4,
       date: '2019/09/16',
       numOunces: 30,
     },
     {
-      userId: 4,
+      userID: 4,
       date: '2019/09/15',
       numOunces: 30,
     },
