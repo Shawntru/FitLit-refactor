dd1552977da1cc1de34cb8f4d3a1067bb3656fa7
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-10-30 20:37:37 -0600

    Add functionality to post new sleep, activity and hydration data

--- src/fetch.js
+++ src/fetch.js
@@ -44,8 +44,42 @@ let requests = {
    .then(response => response.json())
    .then(data => console.log(data))
    .catch(error => console.log(error))
- },
+   },
  
+ postSleepData(inputId, inputDate, inputHoursSlept, inputSleepQuality) {
+   return fetch('	https://fe-apps.herokuapp.com/api/v1/fitlit/1908/sleep/sleepData', {
+     method: 'POST',
+     headers: {
+      'Content-Type': 'application/json'
+     },
+     body: JSON.stringify({
+       "userID": inputId,
+       "date": inputDate,
+       "hoursSlept": inputHoursSlept,
+       "sleepQuality": inputSleepQuality
+     })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+  },
+
+  postHydrationData(inputId, inputDate, inputNumOunces) {
+    return fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/hydration/hydrationData', {
+      method: 'POST',
+      headers: {
+       'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "userID": inputId,
+        "date": inputDate,
+        "numOunces": inputNumOunces
+      })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+  },
 }
 
 export default requests;
